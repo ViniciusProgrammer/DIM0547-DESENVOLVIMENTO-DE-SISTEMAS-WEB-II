c362ff4cb8eb3981c35b3519162ae7a87044e6d0
ViniciusProgrammer <viniciusimdufrn@gmail.com>
2026-04-23 14:32:58 -0300

    feat: Adicionado mais testes unitários

--- interno/repository/dados_test.go
+++ interno/repository/dados_test.go
@@ -19,3 +19,19 @@ func TestAdicionarAluno(t *testing.T) {
 		t.Errorf("Erro: Esperava o nome 'Teste da Silva', mas recebeu '%s'", alunoSalvo.Nome)
 	}
 }
+
+func TestBuscarPorIDExistente(t *testing.T) {
+    repo := &AlunoRepository{}
+    aluno, ok := repo.BuscarPorID(1)
+    if !ok || aluno.ID != 1 {
+        t.Error("deveria encontrar o aluno ID 1")
+    }
+}
+
+func TestBuscarPorIDInexistente(t *testing.T) {
+    repo := &AlunoRepository{}
+    _, ok := repo.BuscarPorID(999)
+    if ok {
+        t.Error("não deveria encontrar aluno com ID 999")
+    }
+}
