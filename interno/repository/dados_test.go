5c8cc600401116fa09f0cea42311f231ec790c24
ViniciusProgrammer <viniciusimdufrn@gmail.com>
2026-04-23 13:56:15 -0300

    refactor(testes): removvendo parametros das funcoes de testes

--- interno/repository/dados_test.go
+++ interno/repository/dados_test.go
@@ -9,15 +9,12 @@ func TestAdicionarAluno(t *testing.T) {
 	repo := &AlunoRepository{}
 	novoAluno := modelo.Aluno{Nome: "Teste da Silva", Turma: "2026.1"}
 
-	// Executa a função que queremos testar
 	alunoSalvo := repo.Adicionar(novoAluno)
 
-	// Verifica se o ID foi gerado corretamente (maior que zero)
 	if alunoSalvo.ID == 0 {
 		t.Errorf("Erro: Esperava que o aluno recebesse um ID válido, mas recebeu %d", alunoSalvo.ID)
 	}
 
-	// Verifica se o nome foi salvo corretamente
 	if alunoSalvo.Nome != "Teste da Silva" {
 		t.Errorf("Erro: Esperava o nome 'Teste da Silva', mas recebeu '%s'", alunoSalvo.Nome)
 	}
